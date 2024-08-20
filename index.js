6f4115adefe6d9667490595c8e4180a3fdf430d8
Prabin Bohara <prabinbohara10@gmail.com>
2024-08-21 00:19:57 +0545

    feat: ffmpeg streaming feature

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import express from "express"
 import multer from "multer"
 import { v4 as uuidv4} from "uuid"
 import path from "path"
+import fs from "fs"
+import {exec} from "child_process"
+import { error } from "console"
+import { stderr } from "process"
 
 const app = express()
 
@@ -33,9 +37,40 @@ app.get('/', function(req, res){
 })
 
 app.post("/upload", upload.single('file'), function(req, res){
-    console.log("file is uploaded")
-    res.json({
-        msg:"File uploaded"
+    const lessonId = uuidv4()
+    const videoPath = req.file.path
+    const outputPath = `./media/courses/${lessonId}`
+    const hlsPath = `${outputPath}/index.m3u8`
+    
+    console.log("lessonId is ", lessonId)
+    console.log("videoPath is ", videoPath)
+    console.log("hlsPath is ", hlsPath)
+
+    // create if folder doesnot exists:
+    if(!fs.existsSync(outputPath)){
+        fs.mkdirSync(outputPath, {recursive: true})
+    }
+
+    //ffmpeg: takes long time. must implement queing system in production
+    const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath}`;
+    console.log(ffmpegCommand)
+
+    exec(ffmpegCommand, (error, stdout, stderr)=>{
+        if(error){
+            console.log(`exec error: ${error}`)
+        }
+        console.log(`stdout: ${stdout}`)
+        console.log(`stderr: ${stderr}`)
+
+        const videoUrl = `http://localhost/media/courses/${lessonId}/index.m3u8`;
+    
+        console.log("file is uploaded")
+        res.json({
+            msg:"Video converted to HLS format",
+            videoUrl: videoUrl,
+            lessonId: lessonId
+        })
+
     })
 })
 
